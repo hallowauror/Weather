f3c04b22d76cc908a76a32f848300c05c3155e61
Ega Arya <ega0110@gmail.com>
2019-05-23 13:45:31 +0700

    fitur lengkap

--- app/src/main/java/com/example/proyekakhir/Retrofit/OpenWeatherMap.java
+++ app/src/main/java/com/example/proyekakhir/Retrofit/OpenWeatherMap.java
@@ -15,6 +15,11 @@ public interface OpenWeatherMap {
                                                  @Query("appid") String appid,
                                                  @Query("units") String unit);
 
+    @GET("weather")
+    Observable<WeatherResult> getWeatherByCityName(@Query("q")String cityName,
+                                                 @Query("appid") String appid,
+                                                 @Query("units") String unit);
+
     @GET("forecast")
     Observable<WeatherForecastResult> getForecastWeatherByLatLng(@Query("lat")String lat,
                                                                  @Query("lon")String lng,

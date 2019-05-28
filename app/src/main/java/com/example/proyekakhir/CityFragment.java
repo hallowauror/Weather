5c951adffdf79a81ab4994fc53c2866676877693
Ega Arya <ega0110@gmail.com>
2019-05-28 20:40:24 +0700

    kurang notifikasi.

--- app/src/main/java/com/example/proyekakhir/CityFragment.java
+++ app/src/main/java/com/example/proyekakhir/CityFragment.java
@@ -202,6 +202,7 @@ public class CityFragment extends Fragment {
                         txt_sunrise.setText(Common.convertUnixtoHour(weatherResult.getSys().getSunrise()));
                         txt_sunset.setText(Common.convertUnixtoHour(weatherResult.getSys().getSunset()));
                         txt_geo_coord.setText(new StringBuilder(weatherResult.getCoord().toString()).toString());
+                        txt_wind.setText(new StringBuilder(String.valueOf(weatherResult.getWind().getSpeed())).append(" m/s").toString());
 
                         // Display Panel
                         weather_panel.setVisibility(View.VISIBLE);
